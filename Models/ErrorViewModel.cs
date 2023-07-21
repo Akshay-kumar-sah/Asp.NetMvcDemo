c144e128f58f1bd14291fa9bb4b4428eef514020
Akshay-kumar-sah <akki19082001@gmail.com>
2023-07-21 20:22:38 +0530

    Initial Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.NetMvcDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
